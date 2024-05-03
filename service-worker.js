4ab50e9633f2585f256404035f677315706dde87
GhostappsLegacy <157152760+GhostappsLegacy@users.noreply.github.com>
2024-05-03 12:32:39 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,30 @@
+const CACHE_NAME = 'offline-test';
+const urlsToCache = [
+  'index.html',
+  'styles.css',
+  '/icons/icon-192x192.png'
+];
 
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Cache hit - return the response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
